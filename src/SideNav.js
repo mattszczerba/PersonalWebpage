b5b6bdcb3ebfb4e44fba36220e0a333f4628f096
mattszczerba <61034582+mattszczerba@users.noreply.github.com>
2022-01-27 15:44:53 -0500

    alt to images per build errors

--- src/SideNav.js
+++ src/SideNav.js
@@ -52,22 +52,22 @@ export default function SideNav(){
             <ul>
                 <li>
                     <a href="https://www.linkedin.com/in/matt-szczerba-240278140" target="blank" title="linkedin.com">
-                        <img className="navimg" src="https://res.cloudinary.com/dow8www4d/image/upload/v1643235594/Webpage/linkedin.png"/>
+                        <img className="navimg" src="https://res.cloudinary.com/dow8www4d/image/upload/v1643235594/Webpage/linkedin.png" alt="LinkedIn"/>
                     </a>
                 </li>
                 <li>
                     <a href="https://github.com/mattszczerba" target="blank" title="github.com">
-                        <img className="navimg" src="https://res.cloudinary.com/dow8www4d/image/upload/v1643235594/Webpage/github.png" />
+                        <img className="navimg" src="https://res.cloudinary.com/dow8www4d/image/upload/v1643235594/Webpage/github.png" alt="Github"/>
                     </a>
                 </li>
                 <li>
                     <a href="https://www.facebook.com/matt.szczerba/" target="blank" title="Facebook.com">
-                        <img className="navimg" src="https://res.cloudinary.com/dow8www4d/image/upload/v1643235594/Webpage/facebook.png"/>
+                        <img className="navimg" src="https://res.cloudinary.com/dow8www4d/image/upload/v1643235594/Webpage/facebook.png" alt="Facebook"/>
                     </a>
                 </li>
                 <li>
                     <a href="https://www.instagram.com/skwizzerbz/" target="blank" title="instagram.com">
-                        <img className="navimg" src="https://res.cloudinary.com/dow8www4d/image/upload/v1643235595/Webpage/instagram.png"/>
+                        <img className="navimg" src="https://res.cloudinary.com/dow8www4d/image/upload/v1643235595/Webpage/instagram.png" alt="Instagram"/>
                     </a>
                 </li>
             </ul>
