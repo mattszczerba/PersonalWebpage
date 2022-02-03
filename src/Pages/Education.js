baab71a1fe1233c1f70755c5628a3ff6d89d69a9
mattszczerba <61034582+mattszczerba@users.noreply.github.com>
2022-02-03 14:44:37 -0500

    started experience cards

--- src/Pages/Education.js
+++ src/Pages/Education.js
@@ -4,37 +4,37 @@ export default function Education(){
     return (
         <div>
             <div className="card-grid">
-                <div className="educationCard">
-                    <div className="educationCardHeader card-image">
+                <div className="card">
+                    <div className="cardHeader card-image">
                         <img src="https://www2.ccsu.edu/_tmpl_home/img/apply-image.jpg" alt="CCSU"/>
                     </div>
-                    <div className="educationCardHeader">
+                    <div className="cardHeader">
                         Central Connecticut State University
                     </div>
-                    <div className="educationCardBody">
+                    <div className="cardBody">
                         Bachelor’s Degree in Computer Science | 2020
                     </div>
-                    <div className="educationCardFooter"></div>
+                    <div className="cardFooter"></div>
                 </div>
-                <div className="educationCard">
-                    <div className="educationCardHeader card-image">
+                <div className="card">
+                    <div className="cardHeader card-image">
                         <img src="https://res.cloudinary.com/dow8www4d/image/upload/v1643841965/Webpage/tunxis.jpg" alt="TXCC"/>
                     </div>
-                    <div className="educationCardHeader">Tunxis Community College</div>
-                    <div className="educationCardBody">
+                    <div className="cardHeader">Tunxis Community College</div>
+                    <div className="cardBody">
                         Associates Degree in Computer Information Systems | 2014
                     </div>
-                    <div className="educationCardFooter"></div>
+                    <div className="cardFooter"></div>
                 </div>
-                <div className="educationCard">
-                    <div className="educationCardHeader card-image">
+                <div className="card">
+                    <div className="cardHeader card-image">
                         <img src="https://res.cloudinary.com/dow8www4d/image/upload/v1643842092/Webpage/SHS.jpg" alt="SHS"/>
                     </div>
-                    <div className="educationCardHeader">Southington High School</div>
-                    <div className="educationCardBody">
+                    <div className="cardHeader">Southington High School</div>
+                    <div className="cardBody">
                         High School Diploma | 2009
                     </div>
-                    <div className="educationCardFooter"></div>
+                    <div className="cardFooter"></div>
                 </div>
             </div>
         </div>
--- src/Pages/Experience.js
+++ src/Pages/Experience.js
@@ -2,56 +2,87 @@ import React from "react";
 
 export default function Experience(){
     return (<>
-        <div>
-            <h2>Software Developer, Avenir Health  </h2>
-            <small>November 2020 – Present</small>
-            <p>
-                Avenir Health is an international health organization that projects outcomes of health interventions. During my time at Avenir I worked both in back-end and front-end software development for web and desktop applications including:
-            </p>
-            <ul>
-                <li>
-                    Coordinating with modelers, translating analytical health models into user-friendly applications.
-                </li>
-                <li>
-                    Front-end website development using Javascript, React, HTML5, and CSS.
-                </li>
-                <li>
-                    Back-end server development using Python.
-                </li>
-                <li>
-                    Desktop application development using Delphi.
-                </li>
-            </ul>
+    <div>
+        <div className="card-grid">
+            <div className="card">
+                <div className="cardHeader">
+                    Software Developer, Avenir Health
+                </div>
+                <div className="cardBody">
+                    <small>November 2020 – Present</small>
+                    <p>
+                        Avenir Health is an international health organization that projects outcomes of health interventions. During my time at Avenir I worked both in back-end and front-end software development for web and desktop applications including:
+                    </p>
+                    <ul>
+                        <li>
+                            Coordinating with modelers, translating analytical health models into user-friendly applications.
+                        </li>
+                        <li>
+                            Front-end website development using Javascript, React, HTML5, and CSS.
+                        </li>
+                        <li>
+                            Back-end server development using Python.
+                        </li>
+                        <li>
+                            Desktop application development using Delphi.
+                        </li>
+                    </ul>
+                </div>
+                <div className="cardFooter"></div>
+            </div>
+            <div className="card">
+                <div className="cardHeader">
+                    IT Support Technician, Webster Bank
+                </div>
+                <div className="cardBody">
+                    <small>June 2017 – October 2020</small>
+                    <p>
+                        Answering help desk phone calls to remotely troubleshoot general company computer issues as well as password resets. Worked with applications such as SCCM, Active Directory, Citrix, RSA, and other proprietary banking applications.
+                    </p>
+                </div>
+                <div className="cardFooter"></div>
+            </div>
+            <div className="card">
+                <div className="cardHeader">
+                    Networking Technician, Central Connecticut State University
+                </div>
+                <div className="cardBody">
+                    <small>June 2015 – June 2017</small>
+                    <p>
+                    Management of campus networking and telecom infrastructure. Configuration and management of campus switches, wire management, working in Cisco environments such as CUCM, Clean Access, NCS Prime, etc., Cisco VoIP phone support.
+                    </p>
+                </div>
+                <div className="cardFooter"></div>
+            </div>
+            <div className="card">
+                <div className="cardHeader">
+                    IT Onsite Support, Central Connecticut State University
+                </div>
+                <div className="cardBody">
+                    <small>August 2014 – June 2015</small>
+                    <p>
+                        Providing technical support to clients around campus. Desktop support, virus removals, reimaging of workstations, SCCM management, Active Directory, Citrix, deployment of new computers, providing loaner machines, classroom support for media peripherals.
+                    </p>
+                </div>
+                <div className="cardFooter"></div>
+            </div>
+            <div className="card">
+                <div className="cardHeader">
+                    Geek Squad Advanced Repair Agent, Best Buy
+                </div>
+                <div className="cardBody">
+                    <small>April 2011 – August 2014</small>
+                    <ul>
+                        <li>Work with clients on a daily basis troubleshooting computer issues. </li>
+                        <li>Diagnostics of client machines as well as virus removals, general   software issues.</li>
+                        <li>Computer hardware installation</li>
+                        <li>Cell phone screen replacement, troubleshooting, and activation</li>
+                        <li>Manage daily reports and queues</li>
+                    </ul>
+                </div>
+                <div className="cardFooter"></div>
+            </div>
         </div>
-        <div>
-            <h2>IT Support Technician, Webster Bank</h2>
-            <small>June 2017 – October 2020</small>
-            <p>
-                Answering help desk phone calls to remotely troubleshoot general company computer issues as well as password resets. Worked with applications such as SCCM, Active Directory, Citrix, RSA, and other proprietary banking applications.
-            </p>
-        </div>
-        <div>
-            <h2>Networking Technician, Central Connecticut State University</h2>
-            <small>June 2015 – June 2017</small>
-            <p>
-                Management of campus networking and telecom infrastructure. Configuration and management of campus switches, wire management, working in Cisco environments such as CUCM, Clean Access, NCS Prime, etc., Cisco VoIP phone support.
-            </p>
-        </div>
-        <div>
-            <h2>IT Onsite Support, Central Connecticut State University</h2>
-            <small>August 2014 – June 2015</small>
-            <p>Providing technical support to clients around campus. Desktop support, virus removals, reimaging of workstations, SCCM management, Active Directory, Citrix, deployment of new computers, providing loaner machines, classroom support for media peripherals.</p>
-        </div>
-        <div>
-            <h2>Geek Squad Advanced Repair Agent, Best Buy</h2>
-            <small>April 2011 – August 2014</small>
-            <ul>
-                <li>Work with clients on a daily basis troubleshooting computer issues. </li>
-                <li>Diagnostics of client machines as well as virus removals, general   software issues.</li>
-                <li>Computer hardware installation</li>
-                <li>Cell phone screen replacement, troubleshooting, and activation</li>
-                <li>Manage daily reports and queues</li>
-            </ul>
-        </div>
+    </div>
     </>);
 }
