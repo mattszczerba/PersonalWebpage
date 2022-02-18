9275f26a55c8618f1f9be5a753a0ab4828e02967
mattszczerba <61034582+mattszczerba@users.noreply.github.com>
2022-02-18 15:14:53 -0500

    Modified about page, added form to contact page.

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -9,7 +9,7 @@ export default function About(){
                 </h2>
             </blockquote>
             <p>
-                I’ve always sought out challenges and opportunities that are meaningful to me. Professionally and in my personal life, there has always an undying appetite to explore, learn, and expand my skill-base. From building a Jeep from the ground up, not knowing a thing about cars getting into it, to taking on software engineering after years of being the artistic one throughout high school, I’ve been willing to conquer any challenge with unequivocal determination. 
+                I’ve always sought out challenges and opportunities that are meaningful to me. Professionally and in my personal life, there has always an undying appetite to explore, learn, and expand my skill-base. I’ve always been willing to conquer any challenge with unequivocal determination. 
             </p>
             <p>
                 As a software developer, I enjoy using my obsessive attention to detail and my love for creating things to help make a difference in this world. I am excited to make an impact with a like-minded team. 
--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,10 +1,56 @@
 import React from "react";
+import { useForm, ValidationError } from '@formspree/react';
 
 export default function Contact(){
+    const [state, handleSubmit] = useForm("xlezbwaj");
     return (
         <div>
-            <h2>Contact: </h2>
-            <a href = "mailto: mattszczerba@outlook.com">mattszczerba@outlook.com</a>
+            <div>
+                <h2>Contact: </h2>
+                <p>Feel free to contact me via my email address or form below:</p>
+                <a href = "mailto: mattszczerba@outlook.com">mattszczerba@outlook.com</a><br/>
+            </div>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="name">
+                      Name:
+                    </label>
+                    <input
+                      id="name"
+                      type="name" 
+                      name="name"
+                    />
+                    <label htmlFor="email">
+                      Email Address:
+                    </label>
+                    <input
+                      id="email"
+                      type="email" 
+                      name="email"
+                    />
+                    <ValidationError 
+                      prefix="Email" 
+                      field="email"
+                      errors={state.errors}
+                    />
+                    <label htmlFor="message">
+                      Message:
+                    </label>
+                    <textarea
+                      id="message"
+                      name="message"
+                    />
+                    <ValidationError 
+                      prefix="Message" 
+                      field="message"
+                      errors={state.errors}
+                    />
+                    <button type="submit" disabled={state.submitting}>
+                      Submit
+                    </button>
+                </form>
+            </div>
+            {state.succeeded && <p>Message sent!</p>}
         </div>
     );
 }
