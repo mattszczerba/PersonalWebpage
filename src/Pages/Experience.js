f90704722687cb4d84eb40b109a82f8d81f00103
mattszczerba <61034582+mattszczerba@users.noreply.github.com>
2022-02-18 12:29:57 -0500

    .

--- src/Pages/Experience.js
+++ src/Pages/Experience.js
@@ -9,7 +9,6 @@ export default function Experience(){
                     Software Developer, Avenir Health
                 </div>
                 <div className="cardBody">
-                    <small>November 2020 – Present</small>
                     <p>
                         Avenir Health is an international health organization that projects outcomes of health interventions. During my time at Avenir I worked both in back-end and front-end software development for web and desktop applications including:
                     </p>
@@ -28,59 +27,55 @@ export default function Experience(){
                         </li>
                     </ul>
                 </div>
-                <div className="cardFooter"></div>
+                <div className="cardFooter">November 2020 – Present</div>
             </div>
             <div className="card">
                 <div className="cardHeader">
                     IT Support Technician, Webster Bank
                 </div>
                 <div className="cardBody">
-                    <small>June 2017 – October 2020</small>
                     <p>
                         Answering help desk phone calls to remotely troubleshoot general company computer issues as well as password resets. Worked with applications such as SCCM, Active Directory, Citrix, RSA, and other proprietary banking applications.
                     </p>
                 </div>
-                <div className="cardFooter"></div>
+                <div className="cardFooter">June 2017 – October 2020</div>
             </div>
             <div className="card">
                 <div className="cardHeader">
                     Networking Technician, Central Connecticut State University
                 </div>
                 <div className="cardBody">
-                    <small>June 2015 – June 2017</small>
                     <p>
                     Management of campus networking and telecom infrastructure. Configuration and management of campus switches, wire management, working in Cisco environments such as CUCM, Clean Access, NCS Prime, etc., Cisco VoIP phone support.
                     </p>
                 </div>
-                <div className="cardFooter"></div>
+                <div className="cardFooter">June 2015 – June 2017</div>
             </div>
             <div className="card">
                 <div className="cardHeader">
                     IT Onsite Support, Central Connecticut State University
                 </div>
                 <div className="cardBody">
-                    <small>August 2014 – June 2015</small>
                     <p>
                         Providing technical support to clients around campus. Desktop support, virus removals, reimaging of workstations, SCCM management, Active Directory, Citrix, deployment of new computers, providing loaner machines, classroom support for media peripherals.
                     </p>
                 </div>
-                <div className="cardFooter"></div>
+                <div className="cardFooter">August 2014 – June 2015</div>
             </div>
             <div className="card">
                 <div className="cardHeader">
                     Geek Squad Advanced Repair Agent, Best Buy
                 </div>
                 <div className="cardBody">
-                    <small>April 2011 – August 2014</small>
                     <ul>
-                        <li>Work with clients on a daily basis troubleshooting computer issues. </li>
+                        <li>Working with clients on a daily basis troubleshooting computer issues. </li>
                         <li>Diagnostics of client machines as well as virus removals, general   software issues.</li>
                         <li>Computer hardware installation</li>
                         <li>Cell phone screen replacement, troubleshooting, and activation</li>
                         <li>Manage daily reports and queues</li>
                     </ul>
                 </div>
-                <div className="cardFooter"></div>
+                <div className="cardFooter">April 2011 – August 2014</div>
             </div>
         </div>
     </div>
