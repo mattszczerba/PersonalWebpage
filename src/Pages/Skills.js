4b611d0530018374153f1f4484acd70730f8362b
mattszczerba <61034582+mattszczerba@users.noreply.github.com>
2022-02-22 15:14:52 -0500

    created cards on skills page

--- src/Pages/Skills.js
+++ src/Pages/Skills.js
@@ -3,14 +3,130 @@ import React from "react";
 export default function Skills(){
     return (
         <div>
-            <ul>
-                <li>Experience in web, server, and desktop application development using Javascript, React,
-                    Angular, HTML5, CSS, Python, Delphi, Java, and other languages. </li>
-                <li>Technical knowledge of computer hardware and software with experience in computer troubleshooting and repair. </li>
-                <li>Experience in network and telecom administration.</li>
-                <li>Also familiar with languages including: Visual Basic, C, C++, SQL.</li>
-                <li>Knowledge of Windows and Linux systems, Microsoft Office, Citrix, Cisco systems, Active Directory.</li>
-            </ul>
+            <div className="cgskills">
+                <div className="card">
+                    <div className="cardHeader">
+                        Front-End Web Development
+                    </div>
+                    <div className="cardBody">
+                        <div className="row text-center">
+                            <div className="column">
+                                <img src="https://res.cloudinary.com/dow8www4d/image/upload/v1645551776/Webpage/javascript.png"
+                                 alt=""
+                                 className="skillImg"/>Javascript
+                            </div>
+                            <div className="column">
+                                <img src="https://res.cloudinary.com/dow8www4d/image/upload/v1645551787/Webpage/react.png"
+                                 alt=""
+                                 className="skillImg"/>React
+                            </div>
+                            <div className="column">
+                                <img src="https://res.cloudinary.com/dow8www4d/image/upload/v1645551766/Webpage/angular.png"
+                                 alt=""
+                                 className="skillImg"/>Angular
+                            </div>
+                            <div className="column">
+                                <img src="https://res.cloudinary.com/dow8www4d/image/upload/v1645551771/Webpage/css3.png"
+                                 alt=""
+                                 className="skillImg"/>CSS3
+                            </div>
+                            <div className="column">
+                                <img src="https://res.cloudinary.com/dow8www4d/image/upload/v1645551763/Webpage/html5.png"
+                                 alt=""
+                                 className="skillImg"/>HTML5
+                            </div>
+                            <div className="column">
+                                <img src="https://res.cloudinary.com/dow8www4d/image/upload/v1645551783/Webpage/nodejs.png"
+                                 alt=""
+                                 className="skillImg"/>Node JS
+                            </div>
+                            <div className="column">
+                                <img src="https://res.cloudinary.com/dow8www4d/image/upload/v1645551781/Webpage/materialize.png"
+                                 alt=""
+                                 className="skillImg"/>Materialize
+                            </div>
+                            <div className="column">
+                                <img src="https://res.cloudinary.com/dow8www4d/image/upload/v1645551769/Webpage/bootstrap.png"
+                                 alt=""
+                                 className="skillImg"/>Bootstrap
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="cardHeader">
+                        Back-End Web Development
+                    </div>
+                    <div className="cardBody">
+                        <div className="row text-center">
+                            <div className="column">
+                                <img src="https://res.cloudinary.com/dow8www4d/image/upload/v1645558055/Webpage/python.png"
+                                 alt=""
+                                 className="skillImg"/>Python
+                            </div>
+                            <div className="column">
+                                <img src="https://res.cloudinary.com/dow8www4d/image/upload/v1645558259/Webpage/postman.png"
+                                 alt=""
+                                 className="skillImg"/>Postman
+                            </div>
+                        </div>
+                    </div>
+                    <div className="cardFooter">
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="cardHeader">
+                        Desktop Application Development
+                    </div>
+                    <div className="cardBody">
+                        <div className="row text-center">
+                            <div className="column">
+                                <img src="https://res.cloudinary.com/dow8www4d/image/upload/v1645557639/Webpage/java.jpg"
+                                 alt=""
+                                 className="skillImg"/>Java
+                            </div>
+                            <div className="column">
+                                <img src="https://res.cloudinary.com/dow8www4d/image/upload/v1645558335/Webpage/delphi.png"
+                                 alt=""
+                                 className="skillImg"/>Delphi
+                            </div>
+                            <div className="column">
+                                <img src="https://res.cloudinary.com/dow8www4d/image/upload/v1645559512/Webpage/Visual_Studio.png"
+                                 alt=""
+                                 className="skillImg"/>Visual Studio/Basic
+                            </div>
+                        </div>
+                    </div>
+                    <div className="cardFooter">
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="cardHeader">
+                        Miscellaneous Languages
+                    </div>
+                    <div className="cardBody">
+                        <div className="row text-center">
+                            <div className="column">
+                                <img src="https://res.cloudinary.com/dow8www4d/image/upload/v1645560130/Webpage/clogo.png"
+                                 alt=""
+                                 className="skillImg"/>C
+                            </div>
+                            <div className="column">
+                                <img src="https://res.cloudinary.com/dow8www4d/image/upload/v1645560100/Webpage/cplusplus.png"
+                                 alt=""
+                                 className="skillImg"/>C++
+                            </div>
+                            <div className="column">
+                                <img src="https://res.cloudinary.com/dow8www4d/image/upload/v1645559949/Webpage/sql.webp"
+                                 alt=""
+                                 className="skillImg"/>SQL
+                            </div>
+                        </div>
+                    </div>
+                    <div className="cardFooter">
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
