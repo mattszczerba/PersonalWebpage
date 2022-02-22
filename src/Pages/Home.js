ee4923719a67a2998d11828e31d8fb9743bd0541
mattszczerba <61034582+mattszczerba@users.noreply.github.com>
2022-02-22 15:41:51 -0500

    changed home page message

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home(){
     return (
         <div className="bodyGrid">
-            <h1 className="homeHeader">Welcome to my portfolio webpage!</h1>
+            <h1 className="homeHeader">Welcome to my portfolio</h1>
             <div className="portrait">
                 <img
                 src="https://res.cloudinary.com/dow8www4d/image/upload/v1643308919/Webpage/portrait.jpg"
@@ -13,7 +13,7 @@ export default function Home(){
                 alt=""
               />
             </div>
-            <p className="homeGreeting">Please excuse the appearance as this is a current work in progress. Feel free to become aquainted with me. I will be using this webpage as a central destination to showcase my work, experience, and projects.</p>
+            <p className="homeGreeting">My name is Matt Szczerba and I am a full-stack developer. Feel free to become aquainted with me as I will be using this webpage as a central destination to showcase my work, experience, and projects.</p>
         </div>
     );
 }
